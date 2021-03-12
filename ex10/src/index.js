2aca363f812a4872fb5978179ebfe2f9c5bc9a8c
MemicaC <corovic_selma@yahoo.com>
2021-03-12 09:23:27 +0100

    String Variables

---
+++ ex10/src/index.js
@@ -0,0 +1,15 @@
+function main () {
+    // Only change code below this line
+    var myFirstName="Selma";
+    var myLastName="Corovic";
+    var myHomeTown="Sarajevo";
+   // Only change code above this line
+   return {
+       myFirstName,
+       myLastName,
+       myHomeTown
+   };
+}
+
+console.log (main());
+module.exports=main;
